3d146a88a851805a7365a10b5f340560887c260c
ricardoissadesousa <144178510+ricardoissadesousa@users.noreply.github.com>
2024-12-14 23:09:28 -0300

    Portifolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Adiciona um listener para o evento 'load' na janela do navegador.
+// Isso significa que o código dentro da função será executado quando todos os recursos da página.
+window.addEventListener('load', () => {
+
+    // Exibe uma mensagem no console do navegador para indicar que o código foi executado.
+    console.log("Bem-vindo !");
+
+    // Exemplo: Exibe uma mensagem de alerta ao usuário.
+    // Quando o site terminar de carregar, o alerta será mostrado na tela com uma mensagem personalizada.
+    alert("Seja bem-vindo ao meu portfólio! Aqui você encontrará uma amostra dos meus projetos e habilidades como estudante de Sistemas de Informação ");
+});
