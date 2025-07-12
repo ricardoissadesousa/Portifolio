0f177e912cd4e55bc6a3b1ee774d703c7cbb0bcf
ricardoissadeousa <ricardoissadesousa@gmail.com>
2025-07-11 22:42:13 -0300

    Atualizando Portfolio

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-// Adiciona um listener para o evento 'load' na janela do navegador.
-// Isso significa que o código dentro da função será executado quando todos os recursos da página.
 window.addEventListener('load', () => {
-
-    // Exibe uma mensagem no console do navegador para indicar que o código foi executado.
-    console.log("Bem-vindo !");
-
-    // Exemplo: Exibe uma mensagem de alerta ao usuário.
-    // Quando o site terminar de carregar, o alerta será mostrado na tela com uma mensagem personalizada.
-    alert("Seja bem-vindo ao meu portfólio! Aqui você encontrará uma amostra dos meus projetos e habilidades como estudante de Sistemas de Informação ");
+    // Exibe uma mensagem de boas-vindas no console do navegador.
+    // É uma forma mais sutil de saudação do que um alerta.
+    console.log("Seja bem-vindo ao meu portfólio! Explore meus projetos e conheça minhas habilidades.");
 });
